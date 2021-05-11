d592db4eb092dab9f5a6628b3f402892f2e02425
ekiafa <kiafaeftychia@gmail.com>
2021-05-11 18:51:35 +0300

    update dijkstra_a_star.py

--- dijkstra_a_star.py
+++ dijkstra_a_star.py
@@ -1,5 +1,29 @@
-import sys 
+import sys,math 
+import heapq
 
+def dijkstra(pointers_array,source,target):
+    unseen=[]
+    seen=[]
+    for i in pointers_array:
+        spd=math.inf
+        path=[]
+        unseen.append(i)
+    heap=[]
+    heapq.heapify(heap)
+    heapq.heappush(source)
+    while len(heap)!=0:
+      top=heap[0]
+      heapq.heappop(heap[0]) 
+      seen.append(top)
+      if top==target:
+          return path
+      for i in neighbors[top]:
+          if i not in seen:
+              if spd(source,i)>spd(source,top):
+                 spd(source,i)=spd(source,top) 
+                 path
+
+def spd(first_node,second_node):
 
 
 
@@ -7,8 +31,8 @@ import sys
 def main():      
        
         out=sys.argv[1]
-        start=sys.argv[2]
-        finish=sys.argv[3]
+        source=sys.argv[2]
+        target=sys.argv[3]
 
         with open('out.txt' ,mode='r') as pointers:
             for line in pointers:
