6c22a14c96006e1bc3ca6c8dbed75f8ad54840c3
ekiafa <kiafaeftychia@gmail.com>
2021-05-20 16:25:15 +0300

    a_star beginning

--- dijkstra_a_star.py
+++ dijkstra_a_star.py
@@ -123,6 +123,49 @@ def dijkstra(graph,start,goal):
         print('And the path is ' + str(path))
 
 
+def a_star(node_points,graph,start,goal):
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = graph
+    path = []
+    for node in unseenNodes:
+        shortest_distance[node] = math.inf
+    shortest_distance[start] = 0
+    global dijkstra_loops
+    while unseenNodes:
+        dijkstra_loops+=1
+        minNode = None
+        for node in unseenNodes:
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+
+        for neighbor, weight in graph[minNode].items():
+            if weight + shortest_distance[minNode] < shortest_distance[neighbor]:
+                shortest_distance[neighbor] = weight + shortest_distance[minNode]
+                predecessor[neighbor] = minNode
+        unseenNodes.pop(minNode)
+        if minNode==goal:
+            break
+
+    currentNode = goal
+    while currentNode != start:
+        try:
+            path.insert(0,currentNode)
+            currentNode = predecessor[currentNode]
+        except KeyError:
+            print('Path not reachable.')
+            break
+    path.insert(0,start)
+    if shortest_distance[goal] != math.inf:
+        print("Dijkstra")
+        print('Shortest distance is ' + str(shortest_distance[goal]))
+        print('And the path is ' + str(path))
+
+
+
+
 """
 
 def calculate_distances(graph, source,target):
